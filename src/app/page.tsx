2b37aca0468628cb2a623f1250486f1218794a53
Arnab Chakraborty <that.zytact@gmail.com>
2025-09-18 14:23:57 +0530

    enable class sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="font-sans relative min-h-screen">
-      <div className="absolute inset-0 flex flex-col items-center justify-center p-4 pb-16 gap-12 sm:p-20 sm:gap-16">
-        <main className="flex flex-col gap-8 sm:gap-[32px] items-center">
+    <div className="relative min-h-screen font-sans">
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-12 p-4 pb-16 sm:gap-16 sm:p-20">
+        <main className="flex flex-col items-center gap-8 sm:gap-[32px]">
           <h1>JalJan</h1>
         </main>
       </div>
